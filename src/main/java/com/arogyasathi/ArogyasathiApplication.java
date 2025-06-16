9decba032ae011eec1a2808a462e78078812cd76
surendra140 <Sb246@123>
2025-06-16 22:20:53 +0530

    initial commit

---
+++ src/main/java/com/arogyasathi/ArogyasathiApplication.java
@@ -0,0 +1,13 @@
+package com.arogyasathi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArogyasathiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArogyasathiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arogyasathi/ArogyasathiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arogyasathi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArogyasathiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
